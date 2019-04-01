9394984623bf3d0231e28dd832d1ec2483f4da4a
Joel Bartlett <joel@murbar.com>
2019-04-01 18:34:15 -0400

    Add register and users routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,44 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const db = require('../data/db');
+const validate = require('../data/validators');
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {});
+router.post('/register', async (req, res) => {
+  try {
+    let user = req.body;
+    const { errors } = validate.user(user);
+    if (errors) {
+      res.status(400).json({
+        error: errors.details[0].message
+      });
+    } else {
+      const hash = bcrypt.hashSync(user.password, 4); // 12 in production
+      user.password = hash;
+      const newUser = db.users.create(user);
+      // generate token and return to caller
+      res.status(201).json({
+        message: 'User created!'
+        // token
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 
 router.post('/login', (req, res) => {});
 
-router.get('/users', (req, res) => {});
+router.get('/users', async (req, res) => {
+  try {
+    const users = await db.users.getAll();
+    res.status(200).json(users);
+  } catch (error) {
+    // console.log(error);
+    res.status(500).json({ error: 'Cannot get users.' });
+  }
+});
 
-modules.exports = router;
+module.exports = router;
