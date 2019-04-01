796c75dc9a470be31fcd45a80a65a70e7593da3b
Joel Bartlett <joel@murbar.com>
2019-04-01 17:07:49 -0400

    Mock server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+// const auth = require('./routes/auth');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+// server.use('/api', auth);
+
+const port = 4000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server listening on http://localhost:${port} ***\n`);
+});
