bfda8a7a55c258e6f350e6fb06122f5f7c45d382
Joel Bartlett <joel@murbar.com>
2019-04-01 17:07:59 -0400

    Mock auth routes

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const db = require('../data/db');
+
+const router = express.Router();
+
+router.post('/register', (req, res) => {});
+
+router.post('/login', (req, res) => {});
+
+router.get('/users', (req, res) => {});
+
+modules.exports = router;
