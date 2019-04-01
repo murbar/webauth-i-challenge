63fae985b649979a500cf80cb683ced2be0dbcd8
Joel Bartlett <joel@murbar.com>
2019-04-01 18:33:33 -0400

    Enable auth routes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-// const auth = require('./routes/auth');
+const auth = require('./routes/auth');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
-// server.use('/api', auth);
+server.use('/api', auth);
 
 const port = 4000;
 
