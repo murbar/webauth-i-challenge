c9aab9687887180e6c2fa97cee032af21155472d
Joel Bartlett <joel@murbar.com>
2019-04-02 16:02:24 -0400

    Add session middleware

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const sessionConfig = require('./data/session-config.js');
+
 const auth = require('./routes/auth');
 
 const server = express();
@@ -8,6 +11,8 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+server.use(session(sessionConfig));
+
 server.use('/api', auth);
 
 const port = 4000;
